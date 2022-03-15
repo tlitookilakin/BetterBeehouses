4d934bd0f5a647359117ef061e549e5b8e48cc0a
tlitookilakin <swyrlyy@gmail.com>
2022-03-15 01:12:03 -0400

    add context tag support add dandelion add daffodil

--- BetterBeehouses/ILHelper.cs
+++ BetterBeehouses/ILHelper.cs
@@ -438,13 +438,15 @@ namespace BetterBeehouses
         }
         public static bool CompareOperands(object op1, object op2)
         {
-            if (op1 == null || op1.Equals(op2))
+            if (op1 == null || op1 is Label || op1.Equals(op2))
                 return true;
 
+            if (op1 is sbyte bt && op2 is int bt2)
+                return Convert.ToInt32(bt) == bt2;
+
             if (op1 is LocalBuilder oper1 && op2 is ValueTuple<int, Type> oper2)
-            {
                 return (oper2.Item1 < 0 || oper1.LocalIndex == oper2.Item1) && (oper2.Item2 == null || oper1.LocalType == oper2.Item2);
-            }
+
             return false;
         }
     }
