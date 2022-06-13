38cfcba5f8bf836ed068fff6898cf57952628574
tlitookilakin <swyrlyy@gmail.com>
2022-06-12 20:38:49 -0400

    PFM compat v2

--- BetterBeehouses/integration/PFMPatch.cs
+++ BetterBeehouses/integration/PFMPatch.cs
@@ -7,47 +7,30 @@ namespace BetterBeehouses.integration
     class PFMPatch
     {
         private static bool isPatched = false;
-        private static object BeehouseDef = null;
-        private static IReflectedField<IDictionary<string, object>> configField = null;
+        private static readonly string[] patchesToPatch = {"performDropDownAction", "DayUpdate", "checkForActionPrefix", "minutesElapsedPrefix"};
         internal static bool Setup()
         {
             if (!ModEntry.helper.ModRegistry.IsLoaded("Digus.ProducerFrameworkMod"))
                 return false;
 
-            var target = AccessTools.TypeByName("ProducerFrameworkMod.Controllers.ProducerController").MethodNamed("AddConfigToRepository");
-
-            if (configField is null)
-                configField = ModEntry.helper.Reflection.GetField<IDictionary<string, object>>(
-                    AccessTools.TypeByName("ProducerFrameworkMod.Controllers.ProducerController"),
-                    "ConfigRepository");
-
-            var configs = configField.GetValue();
+            ModEntry.monitor.Log("PFM Integration " + (isPatched ? "Disabling" : "Enabling"));
+            var type = AccessTools.TypeByName("ProducerFrameworkMod.ObjectOverrides");
 
             if (!isPatched && ModEntry.config.PatchPFM)
             {
-                if (configs.TryGetValue("Bee House", out var c))
-                    BeehouseDef = c;
-                configs.Remove("Bee House");
-                ModEntry.harmony.Patch(target, postfix: new(typeof(PFMPatch), "Postfix"));
+                for(int i = 0; i < patchesToPatch.Length; i++)
+                    ModEntry.harmony.Patch(type.MethodNamed(patchesToPatch[i]), prefix: new(typeof(PFMPatch), nameof(Prefix)));
                 isPatched = true;
-            } else if(isPatched && !ModEntry.config.PatchPFM){
-                if (BeehouseDef is not null)
-                    configs.Add("Bee House", BeehouseDef);
-                ModEntry.harmony.Unpatch(target, HarmonyPatchType.Postfix, ModEntry.ModID);
+            } else if(isPatched && !ModEntry.config.PatchPFM)
+            {
+                for(int i = 0; i < patchesToPatch.Length; i++)
+                    ModEntry.harmony.Unpatch(type.MethodNamed(patchesToPatch[i]), HarmonyPatchType.Prefix, ModEntry.ModID);
                 isPatched = false;
             }
 
             return true;
         }
-        internal static void Postfix()
-        {
-            var configs = configField.GetValue();
-            if (!configs.TryGetValue("Bee House", out var c))
-                return;
-
-            ModEntry.monitor.Log(ModEntry.helper.Translation.Get("general.removedPfmBeehouse"), LogLevel.Info);
-            BeehouseDef = c;
-            configs.Remove("Bee House");
-        }
+        private static bool Prefix(object[] __args, ref bool __result)
+            => !(__result = ((StardewValley.Object)__args[0]).Name == "Bee House");
     }
 }
