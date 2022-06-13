93ad46ee59529de39a24554a67f221f8cd5500e9
tlitookilakin <swyrlyy@gmail.com>
2022-06-12 20:01:38 -0400

    pfm v1

--- BetterBeehouses/integration/PFMPatch.cs
+++ BetterBeehouses/integration/PFMPatch.cs
@@ -1,37 +1,53 @@
 ﻿using HarmonyLib;
 using StardewModdingAPI;
+using System.Collections.Generic;
 
 namespace BetterBeehouses.integration
 {
     class PFMPatch
     {
         private static bool isPatched = false;
+        private static object BeehouseDef = null;
+        private static IReflectedField<IDictionary<string, object>> configField = null;
         internal static bool Setup()
         {
             if (!ModEntry.helper.ModRegistry.IsLoaded("Digus.ProducerFrameworkMod"))
                 return false;
 
-            var target = AccessTools.TypeByName("ProducerFrameworkMod.Controllers.ProducerController").MethodNamed("ValidateConfigProducerName");
+            var target = AccessTools.TypeByName("ProducerFrameworkMod.Controllers.ProducerController").MethodNamed("AddConfigToRepository");
+
+            if (configField is null)
+                configField = ModEntry.helper.Reflection.GetField<IDictionary<string, object>>(
+                    AccessTools.TypeByName("ProducerFrameworkMod.Controllers.ProducerController"),
+                    "ConfigRepository");
+
+            var configs = configField.GetValue();
 
             if (!isPatched && ModEntry.config.PatchPFM)
             {
-                isPatched = false;
+                if (configs.TryGetValue("Bee House", out var c))
+                    BeehouseDef = c;
+                configs.Remove("Bee House");
                 ModEntry.harmony.Patch(target, postfix: new(typeof(PFMPatch), "Postfix"));
                 isPatched = true;
             } else if(isPatched && !ModEntry.config.PatchPFM){
+                if (BeehouseDef is not null)
+                    configs.Add("Bee House", BeehouseDef);
                 ModEntry.harmony.Unpatch(target, HarmonyPatchType.Postfix, ModEntry.ModID);
                 isPatched = false;
             }
 
             return true;
         }
-        private static void Postfix(string producerName, ref bool __result)
+        internal static void Postfix()
         {
-            if(producerName == "Bee House")
-            {
-                ModEntry.monitor.Log(ModEntry.helper.Translation.Get("general.removedPfmBeehouse"), LogLevel.Info);
-                __result = false;
-            }
+            var configs = configField.GetValue();
+            if (!configs.TryGetValue("Bee House", out var c))
+                return;
+
+            ModEntry.monitor.Log(ModEntry.helper.Translation.Get("general.removedPfmBeehouse"), LogLevel.Info);
+            BeehouseDef = c;
+            configs.Remove("Bee House");
         }
     }
 }
